cc86e0256a458befa1d547a560cf2120407fbcc7
Harold Thimbleby <harold@thimbleby.net>
2022-01-27 10:06:16 +0000

    Rebuild of project for PLOS submission

---
+++ expand.js
@@ -0,0 +1,63 @@
+// Harold Thimbleby, 2020-2022
+// Javascript code to expand TeX \input commands
+
+var TeXFiles = ["paper-seb-main.tex", "paper-seb-supplementary-material.tex"];
+
+'use strict';
+
+var fs = require('fs');
+
+function saveFile(fileName, string) {
+    fs.open(fileName, 'w', function (err, fd) {
+        if (err) {
+            if (err.code === 'EEXIST') {
+                console.error(fileName + " can't be over-written");
+                return;
+            }
+            console.error("** Some error trying to write " + fileName);
+            throw err;
+        }
+        fs.writeFileSync(fd, string, function (err) {
+            if (err) {
+                return console.log("** Saving " + fileName + " got error: " + err);
+            }
+        })
+    });
+}
+
+function getFile(file) {
+    try {
+        return fs.readFileSync(file, 'utf8');
+    } catch (err) {
+        console.log("** Can't read file " + file + " - " + err);
+        return "\\error couldn't read " + file + "\n";
+    }
+}
+
+var re = RegExp("\\\\input (.*)\n", "g"), rebib=RegExp("\\\\bibliography{(.*).bib}");
+
+var bufferExpand, bufferInsert;
+
+for (var i = 0; i < TeXFiles.length; i++) {
+    console.log("Recursively expand all \\input and \\bibliography macro calls in " + TeXFiles[i]);
+    bufferExpand = getFile(TeXFiles[i]); // puts in buffer
+    var replaced;
+    do {
+        replaced = false;
+        bufferExpand = bufferExpand.replace(re, function (match, filename) {
+            replaced = true;
+            console.log("        \\input " + filename);
+            bufferInsert = getFile(filename);
+            return "% expand " + filename + "\n" + bufferInsert + "% end expanding " + filename + "\n";
+        });
+        bufferExpand = bufferExpand.replace(rebib, function (match, filename) {
+        	replaced = true;
+            console.log("      * \\bibliography{"+filename+".bib} replaced with " + filename+".bbl");
+            bufferInsert = getFile(filename+".bbl");
+            return "% expand bibliography " + filename + ".bbl\n" + bufferInsert + "\n% end expanding bibliography " + filename + ".bbl\n";
+        });
+    }
+    while (replaced);
+    saveFile("expanded-" + TeXFiles[i], bufferExpand);
+    console.log("  ->  Saved as: " + "expanded-" + TeXFiles[i] + "\n");
+}
